0dd42d8d372b23494f5d4c9edd93d890d7e58e54
velly <velly.dsp@gmail.com>
2014-04-23 22:47:27 +0300

    Velly: create models and some views functions

--- iqfight/iqfight/iqfight_app/models.py
+++ iqfight/iqfight/iqfight_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-import datetime
+import datetime, random
 
 class GameConstants(models.Model):
     questions_in_game   = models.IntegerField(default=10)
@@ -21,8 +21,34 @@ class Answer(models.Model):
     question    = models.ForeignKey(Question)
     
 class Game(models.Model):
+    name        = models.CharField(max_length=50)
     is_active   = models.BooleanField(default=True)
     date_time   = models.DateField()
-    current_question = models.ForeignKey(Question)
+    questions    = models.CommaSeparatedIntegerField(max_length=255)
+    current_question = models.IntegerField(default=0)
+    last_answered   = models.ForeignKey("Player",null=True)
+    num_of_players  = models.IntegerField(default=0)
+    def save(self,*args,**kwargs):
+        if not self.questions:
+            constants = GameConstants.objects.all()[0]
+            lst = Question.objects.order_by('?').values_list("pk",flat=True)[:constants.questions_in_game]
+            self.questions = str(lst)[1:-1]
+        if not self.date_time:
+            self.date_time = datetime.datetime.now()
+        super(self.__class__,self).save(*args,**kwargs)
+
+        
+class Player(models.Model):
+    user    = models.ForeignKey(User)
+    points  = models.IntegerField(default=0)
+    current_game = models.ForeignKey(Game,null=True)
     
+class PlayerGames(models.Model):
+    player  = models.ForeignKey(Player,related_name="games")
+    game    = models.ForeignKey(Game,related_name="players")
+    points  = models.IntegerField(default=0)
+    started = models.DateField()
+    ended   = models.DateField(null=True)
+    
+        
     
--- iqfight/iqfight/iqfight_app/views.py
+++ iqfight/iqfight/iqfight_app/views.py
@@ -1,9 +1,46 @@
 from django.shortcuts import render
+import json
+from django.http import HttpResponse
+from iqfight_app.models import *
+from django.contrib.auth.models import User 
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.decorators import login_required
+import traceback
+from django.core import serializers
 
+def get_response(request,d):
+    return HttpResponse(content=json.dumps(obj=d, encoding="UTF-8"), mimetype='application/json')
 
+def get_games(request,user):
+    return Game.objects.filter(is_active=True).values('name','id')
+    
+def is_logged(request):
+    if request.user.is_anonymous():
+        username = ''
+        session_id = ''
+    else:
+        username = request.user.username
+        session_id = request.COOKIES['sessionid']
+    d = {'username': username, "session_id":session_id}
+    return get_response(request,d)
+    
 is_logged_response  = {
                        'username': '', 'session_id':''
                        }
+def login_func(request):
+    user = authenticate(username=request.POST['username'], password=request.POST['password'])
+    login(request, user)
+    if user:
+        return get_response(request,{"logged":True,
+                                     "session_id":request.COOKIES['sessionid'],
+                                     'username':user.username,
+                                     'games':get_games(request,user)})
+    else:
+        return get_response(request,{"logged":False,
+                                     "session_id":request.COOKIES['sessionid'],
+                                     'username':'',
+                                     'games':[]})
+    
 login_request = {
     'username':'', 'password':''
 }
@@ -26,3 +63,4 @@ play_response = {
 answer_response = {
                     'refresh_interval':1000,'correct':True,'already_answered':False
                    }
+
